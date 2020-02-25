61321ce744ce3bd7a4a684edaee66a1eb5375ffe
gmq <1556382796@qq.com>
2020-02-25 17:30:45 +0800

    idea集成git 第四次测试

---
+++ src/test/java/com/baizhi/Tests.java
@@ -0,0 +1,21 @@
+package com.baizhi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("这是第一次修改");
+        System.out.println();
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+
+}
